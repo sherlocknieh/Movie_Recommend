6e434de13bf9e9a17591cca0bc74c3d383b21122
sherlocknieh <sherlocknieh@gmail.com>
2024-12-23 20:09:11 +0800

    精简代码

--- 数据库/SQLite.py
+++ 数据库/SQLite.py
@@ -7,57 +7,6 @@ input_file1 = '数据库/input/links.csv'
 input_file2 = '数据库/input/movies.csv'
 movies_data = '数据库/output/movies.db'
 similarity_data = '数据库/output/similarities.db'
-demo_data   = '.Test/demo.db'
-
-
-# SQLite 用法示例
-def sqlite_demo():
-
-    # 打开/创建数据库
-    conn = sqlite3.connect(demo_data)
-    
-    # 删除名为 table1 的表格
-    conn.execute('DROP TABLE IF EXISTS table1')
-
-    # 创建名为 table1 的表格
-    conn.execute('''CREATE TABLE IF NOT EXISTS table1 (
-                   id     INT,
-                   name   TEXT,
-                   age    INT
-                   )''')  # table1 表格包含 id, name, age 三列数据
-    
-    # 批量插入数据 (使用 pandas)
-    data = [{'id': 1, 'name': 'John', 'age': 20},
-            {'id': 2, 'name':  None,  'age': 25}]               # 使用字典列表传递数据, 键名要与表格列名一致
-    df = pd.DataFrame(data)                                     # 转换为 DataFrame 对象
-    df.to_sql('table1', conn, if_exists='append', index=False)  # 插入数据到表格
-
-    # 创建游标, 用于逐行处理数据
-    cursor = conn.cursor()
-
-    # 插入数据
-    cursor.execute("INSERT INTO table1 (id, name, age) VALUES (3, 'Mary', 22)")  # 插入 id 为 3 的数据
-
-    # 修改数据
-    cursor.execute('UPDATE table1 SET age = 19 WHERE id = 1')  # 修改 id 为 1 的 age 为 19
-
-    # 删除数据
-    cursor.execute('DELETE FROM table1 WHERE name is NULL')  # 删除 name 列为 NULL 的数据
-
-    # 提交修改
-    conn.commit()
-
-    # 读取数据
-    cursor.execute('SELECT * FROM table1')  # 选中所有行
-    rows = cursor.fetchall()                # 获取所有行
-    for row in rows:  print(row)            # 打印所有行
-
-    # 读取数据 (使用 pandas)
-    df = pd.read_sql_query('SELECT * FROM table1', conn)  # 读取所有数据
-    print(df)
-
-    # 关闭数据库
-    conn.close()
 
 
 # 建立 similarities.db 数据库
@@ -66,10 +15,7 @@ def creat_similarities_sqlite():
     # 打开/创建数据库
     conn = sqlite3.connect(similarity_data)
     
-    # 删除旧的 similarities 表
-    conn.execute('DROP TABLE IF EXISTS similarities')
-
-    # 创建新的 similarities 表
+    # 创建 similarities 空表 (如果已存在则不创建)
     conn.execute('''CREATE TABLE IF NOT EXISTS similarities (
                    movieId1     INT,
                    movieId2     INT,
@@ -82,9 +28,8 @@ def creat_similarities_sqlite():
     # 关闭数据库
     conn.close()  
 
-
 # 建立 movies.db 数据库
-# 1. 合并 links.csv 和 movies.csv 文件到 movies_basic 表
+# 1. 创建 movies_basic 表
 def creat_movies_basic_table():
 
     # 创建 movies.db 数据库并打开
@@ -106,16 +51,13 @@ def creat_movies_basic_table():
     # 关闭数据库
     conn.close()
 
-# 2. 手动创建 movies_detail 表格
+# 2. 创建 movies_detail 表
 def creat_movies_detail_table():
     
     # 打开 movies.db 数据库
-    conn = sqlite3.connect(movies_data)  
+    conn = sqlite3.connect(movies_data)
 
-    # 删除旧的 movies_detail 表
-    conn.execute('DROP TABLE IF EXISTS movies_detail')
-
-    # 新建 movies_detail 表
+    # 新建 movies_detail 表 (如果已存在则不创建)
     conn.execute('''CREATE TABLE IF NOT EXISTS movies_detail (
                    movieId      INT,
                    title_CN     TEXT,
@@ -135,47 +77,28 @@ def creat_movies_detail_table():
 
 # 打印表格数据
 def print_table(file_path,table_name):
-    
     conn = sqlite3.connect(file_path)  # 打开数据库
     df = pd.read_sql_query(f'SELECT * FROM {table_name}', conn)  # 加载所有数据
     print(f'\n[{table_name}]: \n')  # 打印表格名称
-    print(df.head(5))    #打印前 5 行数据
-    print(df.tail(5))    #打印末 5 行数据
+    print(df.head(5))    #打印头 5 行数据
+    print(df.tail(5))    #打印尾 5 行数据
+    conn.close()    # 关闭数据库
 
-    # 关闭数据库
-    conn.close()
-
-# 临时修改表格数据
-def modify_table():
-    
-    file_path = '数据库/movies.db'
-    table_name ='movies_basic'
-    
+# 保存表格数据到 csv 文件
+def save_to_csv(file_path,table_name):
     conn = sqlite3.connect(file_path)  # 打开数据库
-    cursor = conn.cursor()  # 创建游标
-    
-
-    # 删除 movieId 为 1107 的行
-    cursor.execute(f'DELETE FROM {table_name} WHERE movieId = 1107')
-    
-    # 提交修改
-    conn.commit()
-    
-    # 关闭数据库
-    conn.close()
-
+    df = pd.read_sql_query(f'SELECT * FROM {table_name}', conn)  # 加载所有数据
+    df.to_csv(f'.测试/{table_name}.csv', index=False)  # 保存到 csv 文件
+    conn.close()    # 关闭数据库
 
 if __name__ == '__main__':
 
-    #sqlite_demo()  # SQLite 用法示例
-    
-    #modify_table()  # 临时修改表格数据
+    # 建立数据库
+    creat_movies_basic_table()   # 创建 movies_basic 表
+    creat_movies_detail_table()  # 创建 movies_detail 表格
+    creat_similarities_sqlite()  # 建立 similarities.db 数据库
 
-    #creat_movies_basic_table()  # 创建 movies_basic 表
     print_table(movies_data,'movies_basic')  # 查看 movies_basic 表格
-
-    #creat_movies_detail_table()  # 创建 movies_detail 表格
     print_table(movies_data,'movies_detail')  # 查看 movies_detail 表格
-
-    #creat_similarities_sqlite()  # 建立 similarities.db 数据库
     print_table(similarity_data,'similarities')  # 查看 similarities 表格
+    
--- 爬虫/TMDB_API.py
+++ 爬虫/TMDB_API.py
@@ -9,19 +9,20 @@ BASE_URL = "https://api.themoviedb.org/3"
 API_KEY = "49b0d7cb820dde8476b350f3bd577342"
 
 
-# 获取电影详细信息
-def get_movie_details(tmdb_id):
-    """获取电影详细信息"""
-    endpoint = f"{BASE_URL}/movie/{tmdb_id}"
+# 通过 TMDB_API 获取电影信息 (但是是通过 IMDB_ID 进行搜索)
+def get_movie_details(imdb_id):
+
+    endpoint = f"{BASE_URL}/find/tt{imdb_id}"
     params = {
         "api_key": API_KEY,
-        "language": "zh-CN"  # 获取中文信息
+        "language": "zh-CN",
+        "external_source": "imdb_id"
     }
     
     try:
         response = requests.get(endpoint, params=params, timeout=30)
         if response.status_code == 200:
-            data = response.json()
+            data = response.json()['movie_results'][0]
             return {
                 'title_CN': data.get('title'),
                 'release_date': data.get('release_date'),
@@ -31,41 +32,41 @@ def get_movie_details(tmdb_id):
                 'overview': data.get('overview')
             }
         else:
-            print(f"获取电影ID {tmdb_id} 失败: {response.status_code}")
+            print(f"IMDB {imdb_id} 获取失败: {response.status_code}")
             return None
     except Exception as e:
-        print(f"处理电影ID {tmdb_id} 时发生错误: {str(e)}")
+        print(f"IMDB {imdb_id} 发生错误: {str(e)}")
         return None
 
-def main():
 
-    # 加载数据库
-    conn = sqlite3.connect(data_base_path)
-    cursor = conn.cursor()
+
+if __name__ == "__main__":
+
+    # 1. 遍历所有电影ID
+    # 2. 获取电影详细信息 (跳过已爬取的电影)
+    # 3. 保存到数据库
+
+    conn = sqlite3.connect(data_base_path)  # 连接数据库
 
     df1 = pd.read_sql_query("SELECT * FROM movies_basic", conn)    # 读取 movies_basic 表, 获取所有电影ID和TMDBID
     df2 = pd.read_sql_query("SELECT * FROM movies_detail", conn)   # 读取 movies_detail 表, 获取已完成的电影信息
     
     # 遍历 df1 中的每一行
-    for _, row in df1.iterrows():
-        tmdb_id = row['tmdbId']
-        movieid = row['movieId']
-        if movieid in df2['movieId'].values:  # 已有详细信息, 跳过
+    for _, item in df1.iterrows():
+        movieid = item['movieId']
+        if item['movieId'] in df2['movieId'].values:    # 已有详细信息, 跳过
             continue
         else:
-            # 获取详细信息
-            details = get_movie_details(tmdb_id)
+            details = get_movie_details(item['imdbId']) # 调用爬虫获取详细信息
             if details:
                 # 保存详细信息到数据库
-                df = pd.DataFrame([details], columns=list(details.keys()))
-                df['movieId'] = movieid
-                df.to_sql('movies_detail', conn, if_exists='append', index=False)
-                print(f"[+保存成功] [ID:{movieid}]《{details['title_CN']}》({details['release_date'][:4]})\t评分: {details['vote_average']}")
+                df = pd.DataFrame([details], columns=list(details.keys()))           # 转换为 DataFrame
+                df['movieId'] = movieid                                              # 增加 movieId 列
+                df.to_sql('movies_detail', conn, if_exists='append', index=False)    # 保存到 movies_detail 表
+                print(f"[+保存成功] [movieid: {movieid}]《{details['title_CN']}》({details['release_date'][:4]})\t评分: {details['vote_average']}")
             else:
-                print(f"[*保存失败] [ID:{movieid}] ")
+                print(f"[*保存失败] [movieid: {movieid}] IMDB网址: https://www.imdb.com/title/tt{item['imdbId']}/")
                 continue
             time.sleep(0.25)   # 避免请求过快, TMDB_API 限制每10秒钟请求不超过40次
     conn.close()
-
-if __name__ == "__main__":
-    main()
+    print("全部完成")
